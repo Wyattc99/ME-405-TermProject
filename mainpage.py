070a94400bf1ef76e448ff0db6a740f55e18cab6
SpitzJameson <98241947+SpitzJameson@users.noreply.github.com>
2022-02-24 10:00:16 -0800

    mainpage updates

--- mainpage.py
+++ mainpage.py
@@ -6,14 +6,16 @@
 
    @section sec_intro1         Software Design
                                
-                               Our program will consist of tasks position_control1, position_control2, encoder1, encoder2, solenoid_pos, and bumpswitch.
+                               Our program will consist of tasks position_control1, position_control2, encoder1, encoder2, solenoid_pos, and limitswitch.
                                The position control tasks will control the duty cycle of each motor individually and measure how far off in
                                ticks each encoder is from its desired position. As each encoder gets closer to its desired position, the duty
                                cycles will be decreasing to reach a final position without overshooot through closed loop position control.
-                               The encoder tasks will be responsible for updating, reading, and setting encoder position for each motor. The solenoid
-                               task will control wether the solenoid is on or off to move the pencil on and off the paper. Finally, the bumpswitch 
-                               task will read when the pencil has reached the end of the leadscrew, notifying the controller the motor needs to 
-                               stop or switch direction to avoid system failure. 
+                               The encoder tasks will be responsible for updating, reading, and setting encoder position for each motor. A critical part
+                               to this project is finding a way to reset the set position for each motor based on the image we want to draw. We imagine
+                               that we will be using a task that gets the g-code for the desired image, which tells us the set position in the radial and
+                               theta direction for each move of the pencil. Next, the solenoid task will control wether the solenoid is on or off to move 
+                               the pencil on and off the paper. Finally, the limitswitch task will read when the pencil has reached the end of the leadscrew, 
+                               notifying the controller the motor needs to stop or switch direction to avoid system failure. 
                                
                                ALl tasks will be facilitated by a main program that runs each task through a task schedular. Additionally, all
                                shared and queued variables will be created within the main file. We expect each task to share encoder position
@@ -33,11 +35,11 @@
                                Below is the Finite State Machine for our file controlling the set position of the pencil.
                         
                         
-   @author              Jameson Spitz
-   @author              Jacob Wong
-   @author              Wyatt Conner
+   @author                     Jameson Spitz
+   @author                     Jacob Wong
+   @author                     Wyatt Conner
 
-   @copyright           CC BY
+   @copyright                  CC BY
 
-   @date                February 24, 2021
+   @date                       February 24, 2021
 '''
