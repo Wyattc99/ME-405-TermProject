c23304ceb0b4be058fc0c1dacfc07dd38235e3e0
Wyattc99 <wyattconner4399@gmail.com>
2022-02-24 20:26:16 -0800

    yolo

---
+++ docs/navtreeindex0.js
@@ -0,0 +1,10 @@
+var NAVTREEINDEX0 =
+{
+"dir_68267d1309a1af8e8297ef4c3efbcdba.html":[1,0,0],
+"files.html":[1,0],
+"index.html":[],
+"index.html#sec_intro1":[0],
+"index.html#subsec_intro1":[0,0],
+"mainpage_8py.html":[1,0,0,0],
+"pages.html":[]
+};
